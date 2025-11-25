5fa858d396d08253b5293fbf1b0cb7da438c7cde
Devansh-Walia <devanshwalia9898@gmail.com>
2025-11-25 23:50:48 +0530

    links added

---
+++ src/data/constants.ts
@@ -0,0 +1,8 @@
+export const ExternalLinks = {
+  socials: {
+    facebook: "https://www.facebook.com/koshishfamily/",
+    instagram: "https://www.instagram.com/koshish_family",
+    youtube: "https://www.youtube.com/@KoshishFamily",
+    linkedin: "https://www.linkedin.com/company/koshishfamily/",
+  },
+};
