10a83e09073e8e8311924774563fe66839e3ab0b
Devansh-Walia <devanshwalia9898@gmail.com>
2025-12-12 00:54:59 +0530

    compress css and other assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,45 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import svelte from '@astrojs/svelte';
-import tailwindcss from '@tailwindcss/vite';
+import svelte from "@astrojs/svelte";
+import tailwindcss from "@tailwindcss/vite";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  integrations: [
+    svelte(),
+    compress({
+      CSS: true,
+      HTML: true,
+      Image: true,
+      JavaScript: true,
+      SVG: true,
+    }),
+  ],
+
+  build: {
+    inlineStylesheets: "auto",
+    assets: "assets",
+  },
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+    build: {
+      minify: "terser",
+      terserOptions: {
+        compress: {
+          drop_console: true,
+          drop_debugger: true,
+        },
+      },
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vendor: ["astro"],
+          },
+        },
+      },
+    },
+  },
 });
