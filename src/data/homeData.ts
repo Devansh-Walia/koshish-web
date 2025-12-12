9bb74709ed3d7f66e5d87771a475bc10a9ae5d6f
Devansh-Walia <devanshwalia9898@gmail.com>
2025-12-12 23:42:41 +0530

    next event image update

--- src/data/homeData.ts
+++ src/data/homeData.ts
@@ -4,72 +4,209 @@ import { postion } from "../types/enums";
 
 export const hero = {
   title: "Empowering Underprivileged Children",
-  subtitle: "We are dedicated to helping children gain education and develop their overall skills",
-  description: "Volunteer and make a difference in the lives of those who need it most",
+  subtitle:
+    "We are dedicated to helping children gain education and develop their overall skills",
+  description:
+    "Volunteer and make a difference in the lives of those who need it most",
   backgroundImage: "/images/event/event-4.jpg",
   buttonText: "Join Our Team",
-  buttonLink: "https://docs.google.com/forms/d/e/1FAIpQLSd091F7yX0SJMDXT_eLybLfdX69u54_kKmCAwFaqCqXjuo-Dw/viewform"
+  buttonLink:
+    "https://docs.google.com/forms/d/e/1FAIpQLSd091F7yX0SJMDXT_eLybLfdX69u54_kKmCAwFaqCqXjuo-Dw/viewform",
 };
 
 export const kids = [
   {
     name: "Bharti",
-    description: "A college going smart and sweet intelligent girl with a very creative mind. Being the eldest she is always up for any responsibilities and us growing really gracefully.",
+    description:
+      "A college going smart and sweet intelligent girl with a very creative mind. Being the eldest she is always up for any responsibilities and us growing really gracefully.",
     image: "/images/kids/bharti.jpeg",
-    imagePosition: postion.left
+    imagePosition: postion.left,
   },
   {
     name: "Arjun",
-    description: "Arjun, a quite, shy and soft hearted boy always ready to help. Smile is his trademark and it suits him the most. Starting his new phase of life after 12th let's wish a lot of luck.",
+    description:
+      "Arjun, a quite, shy and soft hearted boy always ready to help. Smile is his trademark and it suits him the most. Starting his new phase of life after 12th let's wish a lot of luck.",
     image: "/images/kids/arjun.jpeg",
-    imagePosition: postion.right
+    imagePosition: postion.right,
   },
   {
     name: "Ramesh",
-    description: "Ramesh our mechanical boy and ITI student. Love, Respect, Leadership and Cooperativeness is what makes him loved by all and genuineness is what lies in his heart.",
+    description:
+      "Ramesh our mechanical boy and ITI student. Love, Respect, Leadership and Cooperativeness is what makes him loved by all and genuineness is what lies in his heart.",
     image: "/images/kids/ramesh.jpeg",
-    imagePosition: postion.left
+    imagePosition: postion.left,
   },
   {
     name: "Reena",
-    description: "Our shy girl Reena currently studying in class 9th.A very sweet and simple kid with a very attractive aura which makes her loved by all.She dreams big and hope they'll become true.",
+    description:
+      "Our shy girl Reena currently studying in class 9th.A very sweet and simple kid with a very attractive aura which makes her loved by all.She dreams big and hope they'll become true.",
     image: "/images/kids/reena.jpeg",
-    imagePosition: postion.right
-  }
+    imagePosition: postion.right,
+  },
 ];
 
 export const timelineData = [
-  { year: 2012, title: "प्रारंभिक (Beginning)", details: "Journey started with teaching One kid. Raised Funds for him by selling handmade cards outside College canteen. Gave our efforts a name 'Koshish'", image: "/images/timeline/2012.jpg" },
-  { year: 2013, title: "इक्शणा ( Vision)", details: "Created a Vision of providing education for the underprivileged kids, added more kids and members to Koshish and made it a family.", image: "/images/timeline/2013.jpg" },
-  { year: 2014, title: "साक्षरता (Education)", details: "Started Sending all kids to school taking care of their basic necessities and started celebrating major festivals likes Holi, Diwali, Rakhi, etc.", image: "/images/timeline/2014.jpg" },
-  { year: 2015, title: "उदय (Rise)", details: "Became a fully organized team, recruiting members from all over the city running campaigns like Installation of Dustbins, Distribution Drives and Craft Events.", image: "/images/timeline/2015.jpg" },
-  { year: 2016, title: "संवर्धनम् (Care)", details: "Took one step ahead in taking responsibilities of our kids' health, hygiene, and sanitation. Started organizing Health and Dental Camps for Kids and members.", image: "/images/timeline/2016.jpg" },
-  { year: 2017, title: "वितरणम् (Distribution)", details: "Visited Orphanage for the first time. Started organizing fruits and clothes distribution drives", image: "/images/timeline/2017.jpg" },
-  { year: 2018, title: "सफलता ( Success)", details: "First batch of Kids completed their board exams. Planned Kids Annual Trips to Museums, Zoos, etc", image: "/images/timeline/2018.jpg" },
-  { year: 2019, title: "सहकारिन् ( Collaboration )", details: "Collaboration with other NGOs and societies to help the needy and grow in the process", image: "/images/timeline/2019.jpg" },
-  { year: 2020, title: "आलम्बन ( Foundation)", details: "First Kid sent for Graduation fully sponsored by Koshish Family. Organized Digital classes during Covid Pandemic for all our kids.", image: "/images/timeline/2020.jpg" },
-  { year: 2021, title: "प्रयुक्तिः ( Event)", details: "Awareness Sessions, Summer Camps, Art n Craft Competitions, Yoga Sessions started on a regular basis.", image: "/images/timeline/2021.jpg" },
-  { year: 2022, title: "पाठशाला ( Aashiyana)", details: "Koshish Family Completed 10 years. \"Aashiyana\" a place to study and grow with full amenities was created for Koshish Kids", image: "/images/timeline/2022.jpg" },
-  { year: 2023, title: "पञ्चिका ( Registered)", details: "Koshish Family is registered with high hopes of improving lives with the mission to get formal education for all underprivileged Koshish Kids", image: "/images/timeline/2023.jpg" },
-  { year: 2024, title: "उन्नति (Unnati)", details: "First Kid graduated from Koshish Family after 12 years i.e.Bharti. She did her paid internship as a teacher in Scottish Academy Dehradun. We expanded to other areas of Doon.", image: "/images/timeline/2024.jpeg" },
-  { year: 2025, title: "कौशल (Kaushala)", details: "Skill is the tool for success. Now, Koshish Kids are learning skills at the skill centre for better Job opportunities. Arjun, another graduate in the Koshish Family.", image: "/images/timeline/2025.jpeg" }
+  {
+    year: 2012,
+    title: "प्रारंभिक (Beginning)",
+    details:
+      "Journey started with teaching One kid. Raised Funds for him by selling handmade cards outside College canteen. Gave our efforts a name 'Koshish'",
+    image: "/images/timeline/2012.jpg",
+  },
+  {
+    year: 2013,
+    title: "इक्शणा ( Vision)",
+    details:
+      "Created a Vision of providing education for the underprivileged kids, added more kids and members to Koshish and made it a family.",
+    image: "/images/timeline/2013.jpg",
+  },
+  {
+    year: 2014,
+    title: "साक्षरता (Education)",
+    details:
+      "Started Sending all kids to school taking care of their basic necessities and started celebrating major festivals likes Holi, Diwali, Rakhi, etc.",
+    image: "/images/timeline/2014.jpg",
+  },
+  {
+    year: 2015,
+    title: "उदय (Rise)",
+    details:
+      "Became a fully organized team, recruiting members from all over the city running campaigns like Installation of Dustbins, Distribution Drives and Craft Events.",
+    image: "/images/timeline/2015.jpg",
+  },
+  {
+    year: 2016,
+    title: "संवर्धनम् (Care)",
+    details:
+      "Took one step ahead in taking responsibilities of our kids' health, hygiene, and sanitation. Started organizing Health and Dental Camps for Kids and members.",
+    image: "/images/timeline/2016.jpg",
+  },
+  {
+    year: 2017,
+    title: "वितरणम् (Distribution)",
+    details:
+      "Visited Orphanage for the first time. Started organizing fruits and clothes distribution drives",
+    image: "/images/timeline/2017.jpg",
+  },
+  {
+    year: 2018,
+    title: "सफलता ( Success)",
+    details:
+      "First batch of Kids completed their board exams. Planned Kids Annual Trips to Museums, Zoos, etc",
+    image: "/images/timeline/2018.jpg",
+  },
+  {
+    year: 2019,
+    title: "सहकारिन् ( Collaboration )",
+    details:
+      "Collaboration with other NGOs and societies to help the needy and grow in the process",
+    image: "/images/timeline/2019.jpg",
+  },
+  {
+    year: 2020,
+    title: "आलम्बन ( Foundation)",
+    details:
+      "First Kid sent for Graduation fully sponsored by Koshish Family. Organized Digital classes during Covid Pandemic for all our kids.",
+    image: "/images/timeline/2020.jpg",
+  },
+  {
+    year: 2021,
+    title: "प्रयुक्तिः ( Event)",
+    details:
+      "Awareness Sessions, Summer Camps, Art n Craft Competitions, Yoga Sessions started on a regular basis.",
+    image: "/images/timeline/2021.jpg",
+  },
+  {
+    year: 2022,
+    title: "पाठशाला ( Aashiyana)",
+    details:
+      'Koshish Family Completed 10 years. "Aashiyana" a place to study and grow with full amenities was created for Koshish Kids',
+    image: "/images/timeline/2022.jpg",
+  },
+  {
+    year: 2023,
+    title: "पञ्चिका ( Registered)",
+    details:
+      "Koshish Family is registered with high hopes of improving lives with the mission to get formal education for all underprivileged Koshish Kids",
+    image: "/images/timeline/2023.jpg",
+  },
+  {
+    year: 2024,
+    title: "उन्नति (Unnati)",
+    details:
+      "First Kid graduated from Koshish Family after 12 years i.e.Bharti. She did her paid internship as a teacher in Scottish Academy Dehradun. We expanded to other areas of Doon.",
+    image: "/images/timeline/2024.jpeg",
+  },
+  {
+    year: 2025,
+    title: "कौशल (Kaushala)",
+    details:
+      "Skill is the tool for success. Now, Koshish Kids are learning skills at the skill centre for better Job opportunities. Arjun, another graduate in the Koshish Family.",
+    image: "/images/timeline/2025.jpeg",
+  },
 ];
 
 export const events = [
-  { title: "us next", date: "December 2025", image: "/images/event/diwali-2025.jpg", new: true },
-  { title: "Diwali Celebration", date: "October 2025", image: "/images/event/diwali-2025.jpg" },
-  { title: "Valentines Day Stall", date: "February 2023", image: "/images/event/event-1.jpg" },
-  { title: "Clothes Distribution Drive", date: "February 2023", image: "/images/event/event-2.jpg" },
-  { title: "Clothes Distribution Drive", date: "November 2022", image: "/images/event/event-3.jpg" },
-  { title: "Youthopia", date: "November 2022", image: "/images/event/event-4.jpg" },
-  { title: "Teachers Day", date: "September 2022", image: "/images/event/event-5.jpg" },
-  { title: "Diwali Stall", date: "October 2022", image: "/images/event/event-6.jpg" },
-  { title: "Chat Pooja", date: "October 2022", image: "/images/event/event-7.jpg" },
-  { title: "Youthopia", date: "April 2022", image: "/images/event/event-8.jpg" },
-  { title: "Mussorie Trip", date: "January 2022", image: "/images/event/event-9.jpg" }
+  {
+    title: "up next",
+    date: "December 2025",
+    image: "/images/event/soon.jpg",
+    new: true,
+  },
+  {
+    title: "Diwali Celebration",
+    date: "October 2025",
+    image: "/images/event/diwali-2025.jpg",
+  },
+  {
+    title: "Valentines Day Stall",
+    date: "February 2023",
+    image: "/images/event/event-1.jpg",
+  },
+  {
+    title: "Clothes Distribution Drive",
+    date: "February 2023",
+    image: "/images/event/event-2.jpg",
+  },
+  {
+    title: "Clothes Distribution Drive",
+    date: "November 2022",
+    image: "/images/event/event-3.jpg",
+  },
+  {
+    title: "Youthopia",
+    date: "November 2022",
+    image: "/images/event/event-4.jpg",
+  },
+  {
+    title: "Teachers Day",
+    date: "September 2022",
+    image: "/images/event/event-5.jpg",
+  },
+  {
+    title: "Diwali Stall",
+    date: "October 2022",
+    image: "/images/event/event-6.jpg",
+  },
+  {
+    title: "Chat Pooja",
+    date: "October 2022",
+    image: "/images/event/event-7.jpg",
+  },
+  {
+    title: "Youthopia",
+    date: "April 2022",
+    image: "/images/event/event-8.jpg",
+  },
+  {
+    title: "Mussorie Trip",
+    date: "January 2022",
+    image: "/images/event/event-9.jpg",
+  },
 ];
 
 export const contact = {
   heading: "Connect with US",
-  mapUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3441.17911653095!2d78.0773653!3d30.402659600000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3908d7006ff6fbc9%3A0x790977fcca1cf196!2sKoshish%20Aashiyana!5e0!3m2!1sen!2sin!4v1756048964754!5m2!1sen!2sin"
+  mapUrl:
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3441.17911653095!2d78.0773653!3d30.402659600000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3908d7006ff6fbc9%3A0x790977fcca1cf196!2sKoshish%20Aashiyana!5e0!3m2!1sen!2sin!4v1756048964754!5m2!1sen!2sin",
 };
